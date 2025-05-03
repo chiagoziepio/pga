54423d4f0fe503f61ba4eb04c82a6279c64f61bd
chiagoziepio <nduagubapio@gmail.com>
2025-05-03 15:29:42 +0100

    install shadcn for styling

---
+++ src/lib/fonts.ts
@@ -0,0 +1,12 @@
+import { Open_Sans, Montserrat } from "next/font/google";
+
+export const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+export const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
