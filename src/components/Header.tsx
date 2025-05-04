49285d62ed2d30a859314478bf51ad152da39086
chiagoziepio <nduagubapio@gmail.com>
2025-05-04 15:11:09 +0100

    mobile nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { NavItems } from "@/lib/Datas";
 import Link from "next/link";
 import ConsultationBtn from "./ConsultationBtn";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="  sticky top-[20px] z-50 h-20 font-playfair">
-      <nav className="flex justify-between items-center w-8/12 mx-auto backdrop-blur-md  h-18 bg-bg2/75 rounded-4xl px-2.5 ">
-        <h1 className="font-sans font-bold bg-gradient-to-r from-green-500 via-green-300 to-green-950 bg-clip-text text-transparent text-2xl italic">
+      <nav className="flex justify-between items-center md:w-8/12 w-11/12 mx-auto backdrop-blur-md  h-18 bg-bg2/75 rounded-4xl px-2.5 ">
+        <h1 className="font-sans md:font-bold font-medium bg-gradient-to-r from-green-500 via-green-300 to-green-950 bg-clip-text text-transparent md:text-2xl text-[17px]  italic">
           <Link
             href={"/"}
             className="hover:text-green-500 transition-colors duration-500 ease-in-out"
@@ -25,6 +26,9 @@ const Header = () => {
             </Link>
           ))}
           <ConsultationBtn />
+        </div>
+        <div className="lg:hidden">
+          <MobileNav />
         </div>
       </nav>
     </header>
